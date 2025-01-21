22f111a0874ba34272cf9f2e48e59c4a8e80d9ea
tal shneider <talshn@post.bgu.ac.il>
2025-01-21 20:26:36 +0200

    Small adjustments

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,43 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.srv.Server;
+
+import java.util.InputMismatchException;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Usage: StompServer <port> <tpc/reactor>");
+        }
+
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+            if (port < 1 || port > 65535) {
+                throw new IllegalArgumentException("Port must be between 1 and 65535.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Port must be a valid number.");
+        }
+
+
+        if (args[1].equals("tpc")) {
+            Server.threadPerClient(
+                port,
+                StompProtocol::new,
+                FrameEncoderDecoder::new
+            ).serve();
+        } else if (args[1].equals("reactor")) {
+            Server.reactor(
+                Runtime.getRuntime().availableProcessors(),
+                port,
+                StompProtocol::new,
+                FrameEncoderDecoder::new
+            ).serve();
+        } else {
+            throw new InputMismatchException("Invalid mode. Use 'tpc' or 'reactor'.");
+        }
     }
 }
