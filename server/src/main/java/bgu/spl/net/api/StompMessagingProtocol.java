8ee66fbccb3c191c1aa703a98fe8102dd673979e
tal shneider <talshn@post.bgu.ac.il>
2025-01-21 20:25:25 +0200

    StompProtocol Implementation

--- server/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
+++ server/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
@@ -1,12 +1,14 @@
 package bgu.spl.net.api;
 
+import bgu.spl.net.impl.stomp.ConnectionsImpl;
 import bgu.spl.net.srv.Connections;
 
-public interface StompMessagingProtocol<T>  {
+public interface StompMessagingProtocol<T> extends MessagingProtocol<T>  {
 	/**
 	 * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
 	**/
-    void start(int connectionId, Connections<T> connections);
+    @Override
+    void start(int connectionId, ConnectionsImpl<T> connections);
     
     void process(T message);
 	
