07eb2e4960c7575cd417fe9bf10b3a6d46f1603a
tal shneider <talshn@post.bgu.ac.il>
2025-01-21 20:24:51 +0200

    Changed to use CconnectionsImpl

--- server/src/main/java/bgu/spl/net/api/MessagingProtocol.java
+++ server/src/main/java/bgu/spl/net/api/MessagingProtocol.java
@@ -1,16 +1,17 @@
 package bgu.spl.net.api;
 
+import bgu.spl.net.impl.stomp.ConnectionsImpl;
 import bgu.spl.net.srv.Connections;
 
 public interface MessagingProtocol<T> {
     
-    void start(int connectionId, Connections<T> connections);
+    void start(int connectionId, ConnectionsImpl<T> connections);
     /**
      * process the given message 
      * @param msg the received message
      * @return the response to send or null if no response is expected by the client
      */
-    T process(T msg);
+    void process(T msg);
  
     /**
      * @return true if the connection should be terminated
